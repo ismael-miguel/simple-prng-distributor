2976527f03797b0335bead226346438b8efeca65
ismael-miguel <espertalhao04@hotmail.com>
2015-03-06 17:49:40 +0000

    Update prng.js

--- prng.js
+++ prng.js
@@ -1,6 +1,6 @@
 ( function ( window ) {
 
-    window.RNG = function ( min, max ) {
+	window.RNG = function ( min, max ) {	
 
 		var nan = function ( val ) {
 				/*
@@ -16,65 +16,69 @@
 			},
 			//will contain all the available numbers
 			avail = [];
+
+		if ( nan( min ) )
+		{
+			throw new TypeError( 'Expecting Integer, got ' + ( min != min ? 'NaN' : ( min - 1 == min ? 'Infinity' : typeof min) ) );
+		}
+		else if ( min != min >> 0 )
+		{
+			throw new TypeError( 'Floating-point numbers aren\'t supported' );
+		}
 		
-        if ( nan( min ) )
-        {
-            throw new TypeError( 'Expecting Number or NumericString, got ' + ( min != min ? 'NaN' : ( min - 1 == min ? 'Infinity' : typeof min) ) );
-        }
-        if ( arguments.length > 1 )
-        {
-            if ( nan( max ) )
-            {
-                throw new TypeError( 'Expecting Number or NumericString as the second parameter, got ' + typeof max );
-            }
-			/*
-			 * converts the numbers to integers ( n>>=0 -> n = n >> 0)
-			 * and check if the minimum is higher than the maximum,
-			 * throwing an exception if that is the case
-			 */
-            else if ( ( min >>= 0 ) >= ( max >>= 0 ) )
-            {
-                throw new RangeError( 'The 2nd parameter should be higher than the first' );
-            }
+		if ( arguments.length > 1 )
+		{
+			if ( nan( max ) )
+			{
+				throw new TypeError( 'Expecting Integer as the second parameter, got ' + typeof max );
+			}
+			else if( max != max >> 0 )
+			{
+				throw new TypeError( 'Floating-point numbers aren\'t supported on the 2nd parameter' );
+			}
+			else if ( min >= max )
+			{
+				throw new RangeError( 'The 2nd parameter should be higher than the first' );
+			}
 			else if ( ( max - min ) > 268435455 )
 			{
 				//big numbers crash hard, so we must stop them before
 				throw new RangeError( 'The difference between ' + max + ' and ' + min + ' is higher than ' + 268435455 );
 			}
-        }
-        else
-        {
-            max = min >> 0;
-            min = 0;
-        }
+		}
+		else
+		{
+			max = min >> 0;
+			min = 0;
+		}
 
-        return {
-            valueOf: function ( ) {
-				
-                if( !avail.length )
-                {
-                    for( var i = 0; i <= max - min; i++ )
+		return {
+			valueOf: function ( ) {
+
+				if( !avail.length )
+				{
+					for( var i = 0; i <= max - min; i++ )
 					{
 						avail[i] = min + i;
 					}
-                }
+				}
 
-                if( avail.length > 1 )
-                {
+				if( avail.length > 1 )
+				{
 					//returns a random element between 0 and avail.length (excluded)
-                    return avail.splice( ( window.Math.random() * avail.length ) >> 0 ,1)[0];
-                }
-                else
-                {
-                    return avail.splice(0,1)[0];
-                }
-				
-            },
-            toString: function ( ) {
-                return this.valueOf() + '';
-            }
-        };
+					return avail.splice( ( window.Math.random() * avail.length ) >> 0 ,1)[0];
+				}
+				else
+				{
+					return avail.splice(0,1)[0];
+				}
 
-    };
+			},
+			toString: function ( ) {
+				return this.valueOf() + '';
+			}
+		};
+
+	};
 
 } )( Function('return this')( ) );
